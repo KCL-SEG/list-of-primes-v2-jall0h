07726f30ed1f307d177d7271757f35e7674c13fb
Abu-Bakarr Jalloh <k22010427@kcl.ac.uk>
2023-09-29 16:34:09 +0100

    Add value error

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Number given must be greater than 0")
     list = []
+    count = 0
+    number = 1
+    while count < number_of_primes:
+        factors = []
+        for i in range(1, number + 1):
+            if number % i == 0:
+                factors.append(i)
+        if len(factors) == 2:
+            list.append(number)
+            count += 1
+        number += 1
+
     return list
